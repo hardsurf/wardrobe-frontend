18528b6289415f11726b6d319086267390f2db48
Sviatoslav <svjatik.0632286964@gmail.com>
2019-05-01 23:49:08 +0300

    loginPage

---
+++ login.js
@@ -0,0 +1,45 @@
+
+$(window).on('load', function () {
+
+    $("#log-in").click(function () {
+        const text1 = $("#login").val();
+        const text2 = $("#email").val();
+        const text3 = $("#password").val();
+
+        $.ajax({
+            type: "GET",
+            url: "http://34.76.238.18:8080/users/" + text2,
+            xhrFields: {
+              withCredentials: true
+            },
+            username:text2,
+            password:text3,
+            contentType: 'application/json',
+            success: function () {
+                document.location.href = "index.html";
+            },
+            dataType: "json"
+        });
+    });
+
+    $("#sign-up").click(function () {
+        const text1 = $("#login").val();
+        const text2 = $("#email").val();
+        const text3 = $("#password").val();
+
+        $.ajax({
+            type: "POST",
+            url: "http://34.76.238.18:8080/signup",
+            data: JSON.stringify({
+                "nickName": text1,
+                "email": text2,
+                "password": text3
+            }),
+            contentType: 'application/json',
+            complete : function() {
+              document.location.href = "wardrobe.html";
+            },
+            dataType: "json"
+        });
+    });
+});
